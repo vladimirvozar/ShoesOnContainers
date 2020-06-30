8db60b8a6a2bede76950263cd41c9be02eecdc2e
Vladimir Vozar <vladimir.vozar@sprintingsoftware.com>
2020-07-01 00:23:19 +0200

    Added DBContext migrations, migrated data to dockerized mssql server

--- src/Services/ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalogApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProductCatalogApi.Data;
 
 namespace ProductCatalogApi
 {
@@ -25,6 +27,7 @@ namespace ProductCatalogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
             services.AddControllers();
         }
 
