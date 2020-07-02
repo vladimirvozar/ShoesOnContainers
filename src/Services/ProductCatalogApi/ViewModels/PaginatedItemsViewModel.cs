72cfa2890a7cea13ba56d56449a1fadf43f841fc
Vladimir Vozar <vladimir.vozar@sprintingsoftware.com>
2020-07-02 08:37:14 +0200

    Implemented CatalogController with CRUD actions on catalog items

---
+++ src/Services/ProductCatalogApi/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.ViewModels
+{
+    public class PaginatedItemsViewModel<TEntity> where TEntity : class
+    {
+        public int PageSize { get; private set; }
+        public int PageIndex { get; private set; }
+        public int Count { get; set; }
+        public IEnumerable<TEntity> Data { get; set; }
+
+        public PaginatedItemsViewModel(int pageIndex, int pageSize, int count, IEnumerable<TEntity> data)
+        {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            Count = count;
+            Data = data;
+        }
+    }
+}
