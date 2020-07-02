0f4e304d2e9e6e7aa127a3d4c9a203b4d587b738
Vladimir Vozar <vladimir.vozar@sprintingsoftware.com>
2020-07-02 20:50:13 +0200

    Created "catalog" and "mssqlserver" containers and configured to run with docker-compose

--- src/Services/ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalogApi/Startup.cs
@@ -30,7 +30,15 @@ namespace ProductCatalogApi
         {
             services.Configure<CatalogSettings>(Configuration);
 
-            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
+            // connectionString
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabaseUserPassword"];
+            var connectionString = String.Format("Server={0};Database={1};User Id={2};Password={3};", server, database, user, password);
+
+
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(connectionString));
             
             services.AddControllers();
 
